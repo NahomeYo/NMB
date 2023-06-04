64ae0ee8371b0bd20da092a5d7652a5d41236d59
Nahome Yohannes <121902823+NahomeYo@users.noreply.github.com>
2023-06-03 17:25:12 -0700

    Add files via upload

---
+++ css/scroll.js
@@ -0,0 +1,77 @@
+window.onload = function() {
+    setTimeout(function() {
+      window.scrollTo(0, 0);
+      window.history.scrollRestoration = 'manual';
+      sessionStorage.setItem('scrollPosition', window.scrollY);
+    }, 0);
+  };
+  
+  window.addEventListener('beforeunload', function() {
+    window.history.scrollRestoration = 'auto';
+  });
+  
+  window.addEventListener('unload', function() {
+    sessionStorage.removeItem('scrollPosition');
+  });  
+
+window.addEventListener('scroll', function() {
+    var images = document.querySelectorAll('.logos-header, .flier-header, .pm-header, .brochure-header, .video-editing-header');
+    var windowHeight = window.innerHeight;
+
+    images.forEach(function(img) {
+        var positionFromTop = img.getBoundingClientRect().top;
+
+        if (positionFromTop - windowHeight <= 0) {
+            img.classList.add('swipe-from-left');
+        }
+    });
+});
+
+window.addEventListener('scroll', function() {
+    var images = document.querySelectorAll('.urban-coffee-logo-2, .st-mary-logo-2, .king-tut-logo-2, .smsa-logo-2, .winter-basketball-2, .dental-van-2, .pensoteer-flyer-2, .abouna-brochure, .asd-brochure, .mv-thumbnail, .iom-thumbnail, .ct-thumbnail, .messi, .genesis-poster-2, .pensoteer-poster-red-2, .draft-4');
+    var windowHeight = window.innerHeight;
+
+    images.forEach(function(img) {
+        var positionFromTop = img.getBoundingClientRect().top;
+
+        if (positionFromTop - windowHeight <= 0) {
+            img.classList.add('swipe-from-up');
+        }
+    });
+
+    window.addEventListener('scroll', function() {
+        var images = document.querySelectorAll('.logos-photoshop, .logos-illustrator, .fliers-photoshop fliers, .fliers-illustrator fliers, .pm-photoshop, .brochures-photoshop brochures, .brochures-illustrator brochures, .premiere-pro');
+        var windowHeight = window.innerHeight;
+
+        images.forEach(function(img) {
+            var positionFromTop = img.getBoundingClientRect().top;
+
+            if (positionFromTop - windowHeight <= 0) {
+                img.classList.add('swipe-from-up');
+            }
+        });
+    });
+    
+    window.addEventListener('scroll', function() {
+        const elem = document.querySelector('.desktop .skyline-top');
+        const elemTop = elem.getBoundingClientRect().top;
+        const elemBottom = elem.getBoundingClientRect().bottom;
+        const isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+      
+        if (isVisible) {
+          elem.classList.add('visible');
+        }
+      });
+      
+      window.addEventListener('scroll', function() {
+        const elem = document.querySelector('.desktop .skyline-bottom');
+        const elemTop = elem.getBoundingClientRect().top;
+        const elemBottom = elem.getBoundingClientRect().bottom;
+        const isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+      
+        if (isVisible) {
+          elem.classList.add('visible');
+        }
+      });
+});
+  
